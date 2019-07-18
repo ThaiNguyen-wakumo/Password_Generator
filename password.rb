64ed03ce65398637d1ecaf3e872bbd1285646c28
Thai Nguyen <thainguyen@wakumo.vn>
2019-07-18 10:50:24 +0700

    Reduce nested if statement

--- password.rb
+++ password.rb
@@ -6,16 +6,12 @@ class Password
   DIGIT = ('0'..'9').to_a
   UPPER = ('A'..'Z').to_a
   PUNCT = ('#'..'.').to_a
-  
+
   def self.generate (length: 10, upper: 2, digit: 2, punct: 1, unique: true)
-    if unique == true
-      if digit + punct + upper <= length
-        (UPPER.sample(upper) + DIGIT.sample(digit) + PUNCT.sample(punct) + ALPHA.sample(length-digit-upper-punct)).shuffle.join
-      else
-        return false
-      end
+    if unique == true && digit + punct + upper <= length
+      (UPPER.sample(upper) + DIGIT.sample(digit) + PUNCT.sample(punct) + ALPHA.sample(length-digit-upper-punct)).shuffle.join
     else
       return false
-    end
+    end 
   end
 end
