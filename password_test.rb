fe3dc51c475f9b2f9d18e04541606b8fc2e56fe0
Nhat Nguyen <nhatnguyen@wakumo.vn>
2019-07-18 11:43:19 +0700

    updated

--- password_test.rb
+++ password_test.rb
@@ -3,17 +3,17 @@ require 'minitest/autorun'
 require 'pry'
 
 class PasswordTest < MiniTest::Test
-	def test_uniq
+	def test_unique
  	    assert_equal Password.generate.split('').uniq.size, Password.generate.split('').size
     end 
     
     def test_length
-        assert_equal Password.generate.size, 10
+        assert_equal Password.generate.size, length
     end
 
     def test_digit
-        assert_match (/[0-9]/), Password.generate
-    end
+        assert_match (/[0-9]/), Password.generate, (digit)
+    end	
 
     def test_upper
         assert_match (/[A-Z]/), Password.generate
@@ -24,6 +24,6 @@ class PasswordTest < MiniTest::Test
     end
 
     def test_alpha 
-    	assert_match (/[a-z]/), Password.generate, 5
+    	assert_match (/[a-z]/), Password.generate, (length-upper-punct-digit)
     end 
 end
