1ec75bb1c109c5db9d8b05253feaf3fbf111a5b1
NhatNguyen-wakumo <51897406+NhatNguyen-wakumo@users.noreply.github.com>
2019-07-18 11:53:24 +0700

    Update password_test.rb

--- password_test.rb
+++ password_test.rb
@@ -3,27 +3,30 @@ require 'minitest/autorun'
 require 'pry'
 
 class PasswordTest < MiniTest::Test
-    def test_unique
- 	assert_equal Password.generate.split('').uniq.size, Password.generate.split('').size
-    end 
-    
-    def test_length
-        assert_equal Password.generate.size, 10
-    end
+	def test_unique
+    assert_equal Password.generate.split('').uniq.size, Password.generate.split('').size
+  end  
+  
+  def test_length
+    password = Password.generate(length: 10)
+    assert_equal password.size, 10
+  end
 
-    def test_digit
-        assert_match (/[0-9]/), Password.generate
-    end
+  def test_digit
+    password = Password.generate(length: 10, digit: 2)
+    assert_equal password.size, 10
+    assert_match /[0-9]/, password, 2
+  end
 
-    def test_upper
-        assert_match (/[A-Z]/), Password.generate
-    end
+  def test_upper
+    password = Password.generate(length: 10, upper: 2)
+    assert_equal password.size, 10
+    assert_match /[A-Z]/, password, 2
+  end
 
-    def test_punct
-      assert_match (/[#-.]/), Password.generate
-    end
-
-    def test_alpha 
-    	assert_match (/[a-z]/), Password.generate, 5
-    end 
+  def test_punct
+    password = Password.generate(length: 10, punct: 1)
+    assert_equal password.size, 10
+    assert_match /[#-.]/, password, 1
+  end
 end
